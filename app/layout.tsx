d435e61469dab22b871bf6d97c7c83b9344d3183
sonkun <thaison291004@gmail,com>
2024-10-11 17:06:50 +0700

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import Header from '@/app/components/Header';
 import '@/app/styles/globals.css';
 import { Josefin_Sans } from 'next/font/google';
 
-import Detail from './pages/detail/[id]';
+import Detail from './detail/[id]';
 
 const josefin = Josefin_Sans({
   subsets: ['latin'],
