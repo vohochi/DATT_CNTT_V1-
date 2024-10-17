1c20473b7a6f727048910adbc8d7709aaaf880d4
Vo Ho Chi <chivo241023icloud@gmail.com>
2024-10-18 01:35:22 +0700

    x

--- app/components/SliderMain.tsx
+++ app/components/SliderMain.tsx
@@ -1,19 +1,13 @@
-'use client';
-import Image from 'next/image'; // Import Image component
-
 const SliderMain = () => {
   return (
     <div className="flex flex-row justify-between w-full -mt-[90px] h-auto relative">
       {/* Left Section */}
       <div className="flex flex-col justify-center items-center w-1/2 bg-white min-h-[400px] sm:min-h-[600px] md:min-h-[800px] relative">
         <div className="relative flex flex-col items-start">
-          <Image
-            src="/10003.jpg" // Assuming images are in the 'public' folder
+          <img
+            src="10003.jpg"
             alt="Background"
             className="w-[180px] sm:w-[240px] md:w-[427px] h-auto rounded-lg transition-all"
-            width={427} // Cung cấp chiều rộng cho md
-            height={240} // Cung cấp chiều cao cho md
-            layout="responsive" // Cho phép ảnh tự động điều chỉnh kích thước
           />
           <h1 className="z-20 absolute bottom-0 left-0 z-10 text-[30px] sm:text-[40px] md:text-[60px] uppercase font-bold overflow-hidden whitespace-nowrap">
             Clean Fresh
@@ -35,14 +29,7 @@ const SliderMain = () => {
       {/* Right Section */}
       <div className="flex flex-col justify-center bg-[#d9f1e1] w-1/2 p-4 min-h-[400px] sm:min-h-[600px] md:min-h-[800px] relative">
         <div className="relative ml-[500px]">
-          <Image
-            src="/10004.jpg" // Assuming images are in the 'public' folder
-            alt="Slider Image"
-            className="hidden md:block"
-            width={427} // Cung cấp chiều rộng cho md
-            height={240} // Cung cấp chiều cao cho md
-            layout="responsive" // Cho phép ảnh tự động điều chỉnh kích thước
-          />
+          <img src="10004.jpg" alt="Slider Image" className="hidden md:block" />
         </div>
         <div className="absolute bottom-0 right-[120px] z-10 flex space-x-4 mb-2 ">
           {/* Icon Container */}
@@ -56,13 +43,10 @@ const SliderMain = () => {
             <div className="w-6 h-6 bg-gray-300 rounded-full" />
           </div>
         </div>
-        <Image
+        <img
           src="https://via.placeholder.com/841x832"
           alt="Decorative"
           className="-ml-[50%] sm:-ml-[50%] md:-ml-[20%] w-full sm:w-[1000px] md:w-[1200px] mt-[0] absolute transition-all z-10"
-          width={1200} // Cung cấp chiều rộng cho md
-          height={832} // Cung cấp chiều cao cho md
-          layout="responsive" // Cho phép ảnh tự động điều chỉnh kích thước
         />
       </div>
     </div>
