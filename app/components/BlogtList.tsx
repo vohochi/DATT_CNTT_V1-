7040286728d8111c8a310af6d79c3d784520fb58
NamBiLapTrinh <namdev232004@gmail.com>
2024-12-23 00:56:56 +0700

    blog page, test auto call request cors-anywhere

--- app/components/BlogtList.tsx
+++ app/components/BlogtList.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Card,
   CardBody,
@@ -9,6 +9,9 @@ import {
   Button,
   Pagination,
 } from '@nextui-org/react';
+import { Post } from '@/types/PostBlog';
+import { fetchAllPost } from '@/_lib/post';
+import Link from 'next/link';
 
 // Giả lập dữ liệu blog
 const blogs = Array(20)
@@ -25,41 +28,59 @@ const blogs = Array(20)
   }));
 
 const BlogList = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const blogsPerPage = 6;
 
   const indexOfLastBlog = currentPage * blogsPerPage;
   const indexOfFirstBlog = indexOfLastBlog - blogsPerPage;
-  const currentBlogs = blogs.slice(indexOfFirstBlog, indexOfLastBlog);
+  const currentBlogs = posts.slice(indexOfFirstBlog, indexOfLastBlog);
+
+
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        const { data } = await fetchAllPost();
+        setPosts(data);
+      } catch (error) {
+        console.error('Failed to fetch categories:', error);
+      }
+    };
+
+    getPosts();
+  }, []);
 
   return (
     <div>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 md:gap-8">
         {currentBlogs.map((blog) => (
-          <Card key={blog.id} className="w-full">
-            <CardBody className="p-0">
-              <Image
-                src={blog.image}
-                alt={blog.title}
-                width={570}
-                height={320}
-                className="w-full h-auto object-cover"
-              />
-            </CardBody>
-            <CardFooter className="flex-col items-start p-4 md:p-6">
-              <Button size="sm" color="primary" className="mb-2 text-xs">
-                {blog.category}
-              </Button>
-              <h2 className="text-lg md:text-xl font-bold mb-2">
-                {blog.title}
-              </h2>
-              <p className="text-sm text-gray-600 mb-4">{blog.excerpt}</p>
-              <div className="flex justify-between items-center w-full text-xs md:text-sm">
-                <span className="text-gray-500">BY: {blog.author}</span>
-                <span className="text-gray-500">{blog.date}</span>
-              </div>
-            </CardFooter>
-          </Card>
+          <Link key={blog.id} href={`/blogdetail/${blog.id}`}>
+            <Card className="w-full">
+              <CardBody className="p-0">
+                <Image
+                  src={blog.thumbnail}
+                  alt={blog.title}
+                  width={570}
+                  height={320}
+                  className="w-full h-auto object-cover"
+                />
+              </CardBody>
+              <CardFooter className="flex-col items-start p-4 md:p-6">
+                <Button size="sm" color="primary" className="mb-2 text-xs">
+                  {blog.tags}
+                </Button>
+                <h2 className="text-lg md:text-xl font-bold mb-2">
+                  {blog.title}
+                </h2>
+                <p className="text-sm text-gray-600 mb-4">{blog.slug}</p>
+                <div className="flex justify-between items-center w-full text-xs md:text-sm">
+                  <span className="text-gray-500">BY: {blog.author}</span>
+                  <span className="text-gray-500">{blog.updated_at}</span>
+                </div>
+              </CardFooter>
+            </Card>
+          </Link>
+
         ))}
       </div>
       <div className="flex justify-center mt-8 md:mt-12">
